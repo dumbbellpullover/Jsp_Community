84bffe5b986735985f7e8f6cbe9c273f73f04662
dumbbellpullover <dumbbellpullover@gmail.com>
2023-01-16 18:19:44 +0900

    rq 클래스 도입

---
+++ src/main/java/com/ukj/exam/servlet/GugudanServlet.java
@@ -0,0 +1,27 @@
+package com.ukj.exam.servlet;
+
+import com.ukj.exam.Rq;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    Rq rq = new Rq(req, resp);
+
+    int dan = rq.getIntParam("dan", 9);
+    int limit = rq.getIntParam("limit", 9);
+
+    rq.appendBody("<h1>%d단</h1>".formatted(dan));
+
+    for (int i = 1; i <= limit; i++) {
+      rq.appendBody("<div>%d * %d = %d</div>".formatted(dan, i ,dan * i));
+    }
+  }
+}
