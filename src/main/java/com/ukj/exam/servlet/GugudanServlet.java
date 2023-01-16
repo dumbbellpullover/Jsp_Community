ffa59c91aca63416a31c22842acf5769ce431193
dumbbellpullover <dumbbellpullover@gmail.com>
2023-01-16 18:36:46 +0900

    구구단에 model2ã 방식적용

--- src/main/java/com/ukj/exam/servlet/GugudanServlet.java
+++ src/main/java/com/ukj/exam/servlet/GugudanServlet.java
@@ -1,6 +1,7 @@
 package com.ukj.exam.servlet;
 
 import com.ukj.exam.Rq;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,13 +16,18 @@ public class GugudanServlet extends HttpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     Rq rq = new Rq(req, resp);
 
-    int dan = rq.getIntParam("dan", 9);
-    int limit = rq.getIntParam("limit", 9);
+    int dan = rq.getIntParam("dan", 0);
+    int limit = rq.getIntParam("limit", 0);
 
-    rq.appendBody("<h1>%d단</h1>".formatted(dan));
+    // request에 정보를 담는다.
+    // 왜냐하면 나중에 gugudan2.jsp.jsp에서 해당 내용을 꺼낼 수 있기 떄문에
 
-    for (int i = 1; i <= limit; i++) {
-      rq.appendBody("<div>%d * %d = %d</div>".formatted(dan, i ,dan * i));
-    }
+    req.setAttribute("dan", dan);
+    req.setAttribute("limit", limit);
+
+    // guguda2.jsp 에게 나머지 작업을 토스
+    RequestDispatcher requestDispatcher = req.getRequestDispatcher("/gugudan2.jsp");
+    requestDispatcher.forward(req, resp);
+
   }
 }
